b4082195d8afbb47e8d4b7f41d6f160cc1a98c6b
Ayrat Badykov <ayratin555@gmail.com>
2020-02-20 13:16:07 +0300

    Add token_id to unique index for address_token_balances

--- apps/explorer/priv/repo/migrations/20200209090211_add_token_id_to_token_balances.exs
+++ apps/explorer/priv/repo/migrations/20200209090211_add_token_id_to_token_balances.exs
@@ -8,5 +8,27 @@ defmodule Explorer.Repo.Migrations.AddTokenIdToTokenBalances do
     end
 
     create(index(:address_token_balances, [:token_id]))
+
+    drop(unique_index(:address_token_balances, ~w(address_hash token_contract_address_hash block_number)a))
+
+    drop(
+      unique_index(
+        :address_token_balances,
+        ~w(address_hash token_contract_address_hash block_number)a,
+        name: :unfetched_token_balances,
+        where: "value_fetched_at IS NULL"
+      )
+    )
+
+    create(unique_index(:address_token_balances, ~w(address_hash token_contract_address_hash block_number token_id)a))
+
+    create(
+      unique_index(
+        :address_token_balances,
+        ~w(address_hash token_contract_address_hash block_number token_id)a,
+        name: :unfetched_token_balances,
+        where: "value_fetched_at IS NULL"
+      )
+    )
   end
 end
